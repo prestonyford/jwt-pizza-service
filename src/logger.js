8b09b5a0ca49cc4a44ed55ad806a33ccdc1b1490
Preston Ford <preston.y.ford@gmail.com>
2026-03-23 11:53:07 -0600

    update ci

---
+++ src/logger.js
@@ -0,0 +1,13 @@
+sendLogToGrafana(event) {
+	const body = JSON.stringify(event);
+	fetch(`${config.url}`, {
+		method: 'post',
+		body: body,
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${config.accountId}:${config.apiKey}`,
+		},
+	}).then((res) => {
+		if (!res.ok) console.log('Failed to send log to Grafana');
+	});
+}
