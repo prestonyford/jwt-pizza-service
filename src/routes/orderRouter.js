fefb3089a6386c3343bbb95b2de55621b31ca30b
Preston Ford <preston.y.ford@gmail.com>
2026-04-12 23:19:00 -0600

    Remove chaos testing endpoints, fix logic error in login authentication

--- src/routes/orderRouter.js
+++ src/routes/orderRouter.js
@@ -43,28 +43,6 @@ orderRouter.docs = [
   },
 ];
 
-// Chaos
-let enableChaos = false;
-orderRouter.put(
-  '/chaos/:state',
-  authRouter.authenticateToken,
-  asyncHandler(async (req, res) => {
-    if (req.user.isRole(Role.Admin)) {
-      enableChaos = req.params.state === 'true';
-    }
-
-    res.json({ chaos: enableChaos });
-  })
-);
-
-orderRouter.post('/', (req, res, next) => {
-  if (enableChaos && Math.random() < 0.5) {
-    throw new StatusCodeError('Chaos monkey', 500);
-  }
-  next();
-});
-
-
 // getMenu
 orderRouter.get(
   '/menu',
